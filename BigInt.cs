4f2f126ed5d0f0b7bfe032b458b6bcfd97464cd4
Munteanu Teofil <andreiteofil01@gmail.com>
2023-05-10 22:55:56 +0300

    BigInt init

---
+++ BigInt.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BigIntegerOperations
+{
+    internal class BigInt
+    {
+        public bool Sign { get; set; }
+        public byte[] Value { get; set; }
+
+        public BigInt(string? input)
+        {
+            if (input == null || input.Length == 0) throw new ArgumentNullException("Invalid input");
+
+            input = Regex.Replace(input, @"\s+|[^0-9-]+", string.Empty);
+
+            if (input.StartsWith("-"))
+            {
+                Sign = true;
+                input = input.Substring(1);
+            }
+            input = string.Join("", input.SkipWhile(x => x == '0'));
+
+            Value = new byte[input.Length - 1];
+            Value = input.Select(x => (byte)(x - '0')).ToArray();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BigIntegerOperations;
+
+BigInt x = new BigInt(" -00fdsfd00000\t\n4fdsf334");
+x.Value.ToList().ForEach(x => Console.WriteLine(x));
+Console.WriteLine(x.Sign);
